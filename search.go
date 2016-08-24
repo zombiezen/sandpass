b8bcfab122ef3ff7b62731b728269e425da75d12
Ross Light <ross@zombiezen.com>
2016-08-24 07:32:57 -0700

    Allow multi-word searches
    Words are ANDed together to determine whether a match occurs

--- search.go
+++ search.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"net/http"
+	"unicode"
 
 	"golang.org/x/text/language"
 	textsearch "golang.org/x/text/search"
@@ -35,19 +36,69 @@ func handleSearch(w http.ResponseWriter, r *http.Request) error {
 		Results []*keepass.Entry
 	}
 	data.Query = r.FormValue("q")
-	if data.Query != "" {
-		data.Results = search(db, data.Query)
+	pq := parseQuery(data.Query)
+	if pq == nil {
+		data.Query = ""
+	} else {
+		data.Results = search(db, pq)
 	}
 	return tmpl.ExecuteTemplate(w, "search.html", data)
 }
 
-func search(db *keepass.Database, query string) []*keepass.Entry {
-	pat := textsearch.New(language.Und, textsearch.Loose).CompileString(query)
+func search(db *keepass.Database, q *parsedQuery) []*keepass.Entry {
 	var results []*keepass.Entry
 	for _, e := range db.Entries() {
-		if start, _ := pat.IndexString(e.Title); start != -1 {
+		if q.matchesText(e.Title) {
 			results = append(results, e)
 		}
 	}
 	return results
+}
+
+type parsedQuery struct {
+	pats []*textsearch.Pattern
+}
+
+func parseQuery(query string) *parsedQuery {
+	if len(query) == 0 {
+		return nil
+	}
+	var words []string
+	start := -1
+	for i, r := range query {
+		space := unicode.IsSpace(r)
+		if space && start != -1 {
+			words = append(words, query[start:i])
+			start = -1
+		} else if !space && start == -1 {
+			start = i
+		}
+	}
+	if start != -1 {
+		words = append(words, query[start:])
+	}
+	if len(words) == 0 {
+		return nil
+	}
+	m := textsearch.New(language.Und, textsearch.Loose)
+	if len(words) == 1 {
+		return &parsedQuery{pats: []*textsearch.Pattern{m.CompileString(query)}}
+	}
+	pq := &parsedQuery{pats: make([]*textsearch.Pattern, len(words))}
+	for i := range words {
+		pq.pats[i] = m.CompileString(words[i])
+	}
+	return pq
+}
+
+func (pq *parsedQuery) matchesText(s string) bool {
+	if pq == nil || len(pq.pats) == 0 {
+		return false
+	}
+	for _, pat := range pq.pats {
+		if start, _ := pat.IndexString(s); start == -1 {
+			return false
+		}
+	}
+	return true
 }
