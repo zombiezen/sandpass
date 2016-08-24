d62e96934e7be249e6a05abee4838cb22376b0c7
Ross Light <ross@zombiezen.com>
2016-08-23 22:07:33 -0700

    Remove superfluous searchResult struct

--- search.go
+++ search.go
@@ -23,10 +23,6 @@ import (
 	"zombiezen.com/go/sandpass/pkg/keepass"
 )
 
-type searchResult struct {
-	Entry *keepass.Entry
-}
-
 func handleSearch(w http.ResponseWriter, r *http.Request) error {
 	mu.Lock()
 	defer mu.Unlock()
@@ -36,7 +32,7 @@ func handleSearch(w http.ResponseWriter, r *http.Request) error {
 	}
 	var data struct {
 		Query   string
-		Results []searchResult
+		Results []*keepass.Entry
 	}
 	data.Query = r.FormValue("q")
 	if data.Query != "" {
@@ -45,12 +41,12 @@ func handleSearch(w http.ResponseWriter, r *http.Request) error {
 	return tmpl.ExecuteTemplate(w, "search.html", data)
 }
 
-func search(db *keepass.Database, query string) []searchResult {
+func search(db *keepass.Database, query string) []*keepass.Entry {
 	pat := textsearch.New(language.Und, textsearch.Loose).CompileString(query)
-	var results []searchResult
+	var results []*keepass.Entry
 	for _, e := range db.Entries() {
 		if start, _ := pat.IndexString(e.Title); start != -1 {
-			results = append(results, searchResult{Entry: e})
+			results = append(results, e)
 		}
 	}
 	return results
